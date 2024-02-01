735ee21f2a45cb42de2c487d526bceb844fe1ce6
Jackie <jackietanyen@gmail.com>
2024-02-02 02:17:16 +0800

    Update Sidebar.js

--- client/src/components/layout/Sidebar.js
+++ client/src/components/layout/Sidebar.js
@@ -1,10 +1,18 @@
 import React from 'react';
-import { Box, VStack, HStack, IconButton, useBreakpointValue } from '@chakra-ui/react';
+import {
+  Box,
+  VStack,
+  HStack,
+  IconButton,
+  Image,
+  useBreakpointValue,
+} from '@chakra-ui/react';
 import { FaHome, FaUser, FaCog } from 'react-icons/fa'; // Example icons from react-icons
 
 const Sidebar = () => {
   // This will determine whether to show a vertical or horizontal stack based on the screen size
   const StackComponent = useBreakpointValue({ base: HStack, md: VStack });
+  const logoPath = '/BorderDollarFullLogo.jpeg'; // Replace with the actual path to your logo
 
   return (
     <Box
@@ -17,14 +25,14 @@ const Sidebar = () => {
       right={0}
       bottom={{ base: 0, md: 'auto' }}
       top={{ md: 0 }}
-      width={{ md: "220px" }}
-      height={{ md: "100vh" }}
+      width={{ md: '220px' }}
+      height={{ md: '100vh' }}
       bg="blue.500"
       color="white"
       zIndex="docked"
-      justifyContent={{ base: "space-around", md: "flex-start" }} // Spread items on mobile, stack at the start on desktop
-      borderTopWidth={{ base: "1px", md: 0 }}
-      borderTopColor={{ base: "gray.200", md: "transparent" }}
+      justifyContent={{ base: 'space-around', md: 'flex-start' }} // Spread items on mobile, stack at the start on desktop
+      borderTopWidth={{ base: '1px', md: 0 }}
+      borderTopColor={{ base: 'gray.200', md: 'transparent' }}
     >
       {/* Your sidebar content, which will now adjust based on the breakpoint */}
       <IconButton aria-label="Home" icon={<FaHome />} />
