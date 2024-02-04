77cd08cfbf24c37c676e760f9ac5175c48b46246
Jackie <jackietanyen@gmail.com>
2024-02-05 02:41:37 +0800

    Update PoolsPage.js

--- client/src/pages/PoolsPage.js
+++ client/src/pages/PoolsPage.js
@@ -3,46 +3,43 @@ import {
   Box,
   Flex,
   useBreakpointValue,
-  useColorModeValue,
-  Grid
+  Grid,
 } from '@chakra-ui/react';
 import Header from '../components/layout/Header';
 import Sidebar from '../components/layout/Sidebar';
+import MainContent from '../components/layout/MainContent';
 
 const PoolsPage = () => {
-  const bg = useColorModeValue('gray.100', 'gray.900');
-  const headerBg = useColorModeValue('blue.600', 'blue.900');
-
   // Determine if it's a mobile view
   const isMobile = useBreakpointValue({ base: true, md: false });
 
   return isMobile ? (
     // Mobile view with stacked layout
     <Flex direction="column" h="100vh">
-      <Box bg={headerBg} >
+      <Box>
         <Header />
       </Box>
-      <Box flex="1" bg={bg} p={4}>
+      <Box flex="1" p={4}>
         {/* Your main content goes here */}
-        Main Area
+        <MainContent />
       </Box>
       {/* Sidebar is moved here for mobile view to be at the bottom */}
-      <Box bg={bg} p={4}>
+      <Box p={4}>
         <Sidebar />
       </Box>
     </Flex>
   ) : (
     // Desktop view with grid layout
     <Grid templateColumns="220px 1fr" templateRows="60px 1fr" h="100vh">
-      <Box gridRow="1 / 3" gridColumn="1" bg={bg}>
+      <Box gridRow="1 / 3" gridColumn="1">
         <Sidebar />
       </Box>
-      <Box gridRow="1" gridColumn="2" bg={headerBg}>
+      <Box gridRow="1" gridColumn="2">
         <Header />
       </Box>
-      <Box gridRow="2" gridColumn="2" bg={bg} p={4}>
+      <Box gridRow="2" gridColumn="2" px={{ base: '16px', md: '24px' }}>
         {/* Your main content goes here */}
-        Main Area
+        <MainContent />
       </Box>
     </Grid>
   );
