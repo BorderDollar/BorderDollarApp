11b0af426b52a84bb6c41633b2958945ff8047e0
Jackie <jackietanyen@gmail.com>
2024-02-10 01:12:26 +0800

    Update App.js

--- client/src/App.js
+++ client/src/App.js
@@ -2,6 +2,7 @@ import React from 'react';
 import { ChakraProvider } from '@chakra-ui/react';
 import SignIn from './pages/SignInPage'; 
 import PoolsPage from './pages/PoolsPage';
+import PoolDetailPage from './pages/PoolDetailPage';
 import {
   BrowserRouter as Router,
   Routes,
@@ -18,6 +19,7 @@ function App() {
           <Route path="/signin" element={<SignIn />} />
           <Route path="/pools" element={<PoolsPage />} />
           <Route path="/" element={<Navigate replace to="/signin" />} />
+          <Route path="/pools/:poolId" element={<PoolDetailPage />} />
         </Routes>
       </Router>
     </ChakraProvider>
