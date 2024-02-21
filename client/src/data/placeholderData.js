e24a8e9840254cbec5089d80312b10047528b868
Jackie <jackietanyen@gmail.com>
2024-02-22 00:18:46 +0800

    Create placeholderData.js

---
+++ client/src/data/placeholderData.js
@@ -0,0 +1,16 @@
+// src/data/placeholderData.js
+
+const placeholderPoolData = {
+    "12345": {
+      id: "12345",
+      name: "Anemoy Liquid Treasury Fund 1",
+      assetClass: "US Treasuries",
+      valueLocked: "4,610,342 USDC",
+      averageMaturity: "5.3 months",
+      // Add more details as needed
+    },
+    // You can add more pool data with different IDs as keys
+  };
+  
+  export default placeholderPoolData;
+  
