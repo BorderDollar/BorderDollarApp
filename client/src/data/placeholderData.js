5776cc3afa3b37773ecf924e3566f5b07dc7a8c6
Jackie <jackietanyen@gmail.com>
2024-03-18 12:56:32 +0800

    Update placeholderData.js

--- client/src/data/placeholderData.js
+++ client/src/data/placeholderData.js
@@ -3,11 +3,76 @@
 const placeholderPoolData = {
     "12345": {
       id: "12345",
-      name: "Anemoy Liquid Treasury Fund 1",
-      assetClass: "US Treasuries",
+      name: "404 Dataset Not Found",
+      assetClass: "Not available",
       valueLocked: "4,610,342 USDC",
       averageMaturity: "5.3 months",
       // Add more details as needed
+    },
+    "1": {
+      id: "1",
+      name: 'HS Trading Company',
+      assetClass: 'Trade Invoice',
+      valueLocked: '25,000 USDC',
+      apr: '10.2%',
+      status: 'Open for investments',
+      id: '1'
+    },
+    "2": {
+      id: "2",
+      name: 'Zodiac Delivery Services',
+      assetClass: 'Delivery Order',
+      valueLocked: '100,000 USDC',
+      apr: '15.0%',
+      status: 'Open for investments',
+    },
+    "3": {
+      id: "3",
+      name: 'Ocean Trade Pte. Ltd. Pool',
+      assetClass: 'Trade Invoices',
+      valueLocked: '26,000 USDC',
+      apr: '8.0%',
+      status: 'Open for investments',
+    },
+    "4": {
+      id: "4",
+      name: 'BorderDollar Series II',
+      assetClass: 'Structured Credit',
+      valueLocked: '1,000,000 USDC',
+      apr: '12.0%',
+      status: 'Closed',
+    },
+    "5": {
+      id: "5",
+      name: 'BorderDollar Series I',
+      assetClass: 'Structured Credit',
+      valueLocked: '250,000 USDC',
+      apr: '7.6%',
+      status: 'Closed',
+    },
+    "6": {
+      id: "6",
+      name: 'ALT 1.0 SPV',
+      assetClass: 'Invoice Financing',
+      valueLocked: '680,937 DAI',
+      apr: '5.0%',
+      status: 'Open for investments',
+    },
+    "7": {
+      id: "7",
+      name: 'BlockTower Series 1',
+      assetClass: 'Whole Loans',
+      valueLocked: '0 DAI',
+      apr: '4.0%',
+      status: 'Maker Pool',
+    },
+    "8": {
+      id: "8",
+      name: 'BlockTower Series 2',
+      assetClass: 'Debt Facilities',
+      valueLocked: '0 DAI',
+      apr: '4.0%',
+      status: 'Maker Pool',
     },
     // You can add more pool data with different IDs as keys
   };
