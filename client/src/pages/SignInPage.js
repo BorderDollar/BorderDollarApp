9633d226c2cafe6f1bcf2b8433fcd8fffad8a291
Jackie <jackietanyen@gmail.com>
2024-05-13 02:07:17 +0800

    Update SignInPage.js

--- client/src/pages/SignInPage.js
+++ client/src/pages/SignInPage.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState } from 'react';
 import {
   Box,
   Flex,
@@ -6,18 +6,84 @@ import {
   FormControl,
   FormLabel,
   Input,
-  Checkbox,
   Button,
   Text,
   Grid,
   Image,
+  useToast,
+  Checkbox,
 } from '@chakra-ui/react';
+import { supabase } from '../api/supabaseClient';
 
 export default function SignIn() {
+  const [email, setEmail] = useState('');
+  const [isSigningUp, setIsSigningUp] = useState(false);
+  const [emailSent, setEmailSent] = useState(false); // New state to track if the email link was sent
+  const toast = useToast();
+
+  const handleMagicLinkSignIn = async e => {
+    e.preventDefault();
+    const { error } = await supabase.auth.signInWithOtp({
+      email,
+      options: {
+        emailRedirectTo: process.env.REACT_APP_SUPABASE_REDIRECT_URL,
+      },
+    });
+
+    if (error) {
+      toast({
+        title: 'Error',
+        description: error.message,
+        status: 'error',
+        duration: 9000,
+        isClosable: true,
+      });
+    } else {
+      setEmailSent(true);
+      toast({
+        title: 'Check your email',
+        description: 'A sign-in link has been sent to your email address.',
+        status: 'success',
+        duration: 9000,
+        isClosable: true,
+      });
+    }
+  };
+
+  const handleGoogleLogin = async () => {
+    const { error } = await supabase.auth.signInWithIdToken({
+      provider: 'google',
+    });
+
+    if (error) {
+      toast({
+        title: 'Error logging in with Google',
+        description: error.message,
+        status: 'error',
+        duration: 9000,
+        isClosable: true,
+      });
+    } else {
+      setEmailSent(true);
+      toast({
+        title: 'Google Login Successful',
+        description: 'You have successfully logged in with Google.',
+        status: 'success',
+        duration: 9000,
+        isClosable: true,
+      });
+    }
+  };
+
+  const toggleSignUp = () => {
+    setIsSigningUp(!isSigningUp);
+    setEmailSent(false);
+  };
+
   return (
     <Grid
       minHeight={{ base: '100%', md: '100vh' }}
-      templateColumns={{ base: 'repeat(1, 1fr)' }} // 1 column on base, 2 columns from md up
+      templateColumns={{ base: 'repeat(1, 1fr)' }}
       templateRows="1fr"
       templateAreas={{
         base: `"form"`,
@@ -30,7 +96,7 @@ export default function SignIn() {
         bgImage="url('signin_ship.jpg')"
         bgSize="cover"
         bgPosition="center"
-        display={{ base: 'none', md: 'block' }} // Hide on base, show on md and up
+        display={{ base: 'none', md: 'block' }}
       />
       <Flex w="100%" alignItems="center" direction="column">
         <Flex
@@ -50,48 +116,75 @@ export default function SignIn() {
             w={{ base: '100%', md: '432px' }}
           >
             <Image src="BorderDollarFullLogo.jpeg" w="400px"></Image>
-            <Heading size="md" pt="10px" pb="10px" mt="16px" mb="16px">
-              Sign in to start fueling global trade
-            </Heading>
-            <FormControl id="email" isRequired>
-              <FormLabel>Email address</FormLabel>
-              <Input placeholder="Enter your email" />
-            </FormControl>
-            <Checkbox defaultIsChecked mt={4}>
-              Remember me
-            </Checkbox>
-            <Button width="full" mt={4}>
-              Sign In
-            </Button>
-            <Button
-              alignItems="center"
-              borderColor="orange.300"
-              bg="white"
-              color="blue.500"
-              width="full"
-              borderWidth="1px"
-              mt={6}
-              leftIcon={
-                <img
-                  src="GoogleGLogo.svg"
-                  alt="Google Icon"
-                  style={{ width: '20px' }}
-                />
-              }
-            >
-              Log In with Google
-            </Button>
-            <Text mt={6}>Don't have an account yet?</Text>
-            <Button
-              borderColor="orange.300"
-              bg="white"
-              color="blue.500"
-              width="full"
-              borderWidth="1px"
-              mt={2}
-            >
-              Sign Up Here
-            </Button>
+            {emailSent ? (
+              <Flex direction="column" align="center">
+                <Text fontSize="lg" mt="20px">
+                  {email}
+                </Text>
+                <Text mt="16px">
+                  Check your email, and click on the link to log in!
+                </Text>
+              </Flex>
+            ) : (
+              <>
+                <Heading size="md" pt="10px" pb="10px" mt="16px" mb="16px">
+                  {isSigningUp
+                    ? 'Sign up to start fueling global trade'
+                    : 'Sign in to start fueling global trade'}
+                </Heading>
+                <FormControl id="email" isRequired>
+                  <FormLabel>Email address</FormLabel>
+                  <Input
+                    value={email}
+                    onChange={e => setEmail(e.target.value)}
+                    placeholder="Enter your email"
+                  />
+                </FormControl>
+                {!isSigningUp && (
+                  <Checkbox defaultIsChecked mt={4}>
+                    Remember me
+                  </Checkbox>
+                )}
+                <Button width="full" mt={4} onClick={handleMagicLinkSignIn}>
+                  {isSigningUp ? 'Sign Up' : 'Sign In'}
+                </Button>
+                <Button
+                  onClick={handleGoogleLogin}
+                  alignItems="center"
+                  borderColor="orange.300"
+                  bg="white"
+                  color="blue.500"
+                  width="full"
+                  borderWidth="1px"
+                  mt={6}
+                  leftIcon={
+                    <img
+                      src="GoogleGLogo.svg"
+                      alt="Google Icon"
+                      style={{ width: '20px' }}
+                    />
+                  }
+                >
+                  {isSigningUp ? 'Sign Up with Google' : 'Log In with Google'}
+                </Button>
+                <Text mt={6}>
+                  {isSigningUp
+                    ? 'Already have an account?'
+                    : "Don't have an account yet?"}
+                </Text>
+                <Button
+                  borderColor="orange.300"
+                  bg="white"
+                  color="blue.500"
+                  width="full"
+                  borderWidth="1px"
+                  mt={2}
+                  onClick={toggleSignUp}
+                >
+                  {isSigningUp ? 'Sign In Here' : 'Sign Up Here'}
+                </Button>
+              </>
+            )}
           </Box>
         </Flex>
       </Flex>
