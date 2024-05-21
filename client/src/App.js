3b8ba217505bec777bc05fe6a56b6c70e124de3c
Jackie <jackietanyen@gmail.com>
2024-05-21 15:20:43 +0800

    Update App.js

--- client/src/App.js
+++ client/src/App.js
@@ -4,6 +4,9 @@ import { AuthProvider } from './auth/AuthContext';
 import SignIn from './pages/SignInPage';
 import PoolsPage from './pages/PoolsPage';
 import PoolDetailPage from './pages/PoolDetailPage';
+import AdminDashboard from './pages/admin/AdminDashboard';
+import CreateCampaign from './pages/admin/CreateCampaign';
+import EditCampaign from './pages/admin/EditCampaign';
 import {
   BrowserRouter as Router,
   Routes,
@@ -11,27 +14,30 @@ import {
   Navigate,
 } from 'react-router-dom';
 import theme from './theme/theme';
-import RequireAuth from './auth/RequireAuth'
-import RedirectIfLoggedIn from './auth/RedirectIfLoggedIn'
+import RequireAuth from './auth/RequireAuth';
+import RedirectIfLoggedIn from './auth/RedirectIfLoggedIn';
 
 function App() {
   return (
-      <ChakraProvider theme={theme}>
-          <Router>
-              <AuthProvider>
-                  <Routes>
-                      <Route path="/signin" element={
-                          <RedirectIfLoggedIn>
-                              <SignIn />
-                          </RedirectIfLoggedIn>
-                      } />
-                      <Route path="/pools" element={<RequireAuth><PoolsPage /></RequireAuth>} />
-                      <Route path="/pools/:poolId" element={<RequireAuth><PoolDetailPage /></RequireAuth>} />
-                      <Route path="/" element={<Navigate replace to="/signin" />} />
-                  </Routes>
-              </AuthProvider>
-          </Router>
-      </ChakraProvider>
+    <ChakraProvider theme={theme}>
+      <Router>
+        <AuthProvider>
+          <Routes>
+            <Route path="/signin" element={
+              <RedirectIfLoggedIn>
+                <SignIn />
+              </RedirectIfLoggedIn>
+            } />
+            <Route path="/pools" element={<RequireAuth><PoolsPage /></RequireAuth>} />
+            <Route path="/pools/:poolId" element={<RequireAuth><PoolDetailPage /></RequireAuth>} />
+            <Route path="/admin" element={<RequireAuth><AdminDashboard /></RequireAuth>} />
+            <Route path="/admin/create" element={<RequireAuth><CreateCampaign /></RequireAuth>} />
+            <Route path="/admin/edit/:id" element={<RequireAuth><EditCampaign /></RequireAuth>} />
+            <Route path="/" element={<Navigate replace to="/signin" />} />
+          </Routes>
+        </AuthProvider>
+      </Router>
+    </ChakraProvider>
   );
 }
 
