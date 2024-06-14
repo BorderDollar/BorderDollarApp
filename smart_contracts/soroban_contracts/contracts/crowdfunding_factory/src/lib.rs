6bb2a757e83ea6ef1b5d2f5e50e574cae8cc4403
Jackie <jackietanyen@gmail.com>
2024-06-14 14:11:29 +0800

    Update lib.rs

--- smart_contracts/soroban_contracts/contracts/crowdfunding_factory/src/lib.rs
+++ smart_contracts/soroban_contracts/contracts/crowdfunding_factory/src/lib.rs
@@ -19,9 +19,10 @@ impl Deployer {
         deployer: Address,
         wasm_hash: BytesN<32>,
         salt: BytesN<32>,
-        init_fn: Symbol,
-        init_args: Vec<Val>
-    ) -> (Address, Val) {
+        // init_fn: Symbol,
+        // init_args: Vec<Val>
+    // ) -> (Address, Val) {
+    ) -> Address {
         // Skip authorization if deployer is the current contract.
         if deployer != env.current_contract_address() {
             deployer.require_auth();
@@ -34,9 +35,10 @@ impl Deployer {
             .deploy(wasm_hash);
 
         // Invoke the init function with the given arguments.
-        let res: Val = env.invoke_contract(&deployed_address, &init_fn, init_args);
+        // let res: Val = env.invoke_contract(&deployed_address, &init_fn, init_args);
         // Return the contract ID of the deployed contract and the result of
         // invoking the init result.
-        (deployed_address, res)
+        // (deployed_address, res)
+        deployed_address
     }
 }
