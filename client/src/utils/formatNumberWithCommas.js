e266061d7d856ea9fd565ea0baad751027363192
Jackie <jackietanyen@gmail.com>
2024-06-18 16:20:31 +0800

    Delete formatNumberWithCommas.js

--- client/src/utils/formatNumberWithCommas.js
+++
@@ -1,6 +0,0 @@
-// src/utils/formatNumberWithCommas.js
-const formatNumberWithCommas = number => {
-    return number.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ',');
-  };
-  
-  export default formatNumberWithCommas;  
