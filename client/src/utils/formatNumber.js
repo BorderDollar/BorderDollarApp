f99d6a05b1751b2c556691f8900825adbe990673
Jackie <jackietanyen@gmail.com>
2024-06-21 00:55:25 +0800

    Update formatNumber.js

--- client/src/utils/formatNumber.js
+++ client/src/utils/formatNumber.js
@@ -6,3 +6,10 @@ export const formatNumberWithCommas = number => {
 export const formatNumberWithTwoDecimalPlaces = number => {
   return (Math.round(number * 100) / 100).toFixed(2);
 }
+
+export const formatDateIntoUnix = dateString => {
+  const [year, month, day] = dateString.split('-').map(Number);
+  const date = new Date(year, month - 1, day);
+  const unixTimeSeconds = Math.floor(date.getTime() / 1000);
+  return unixTimeSeconds
+}
