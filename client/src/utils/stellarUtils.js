d6f770e54a5463929d6485a55c23031899d2ac1d
Jackie <jackietanyen@gmail.com>
2024-06-24 02:11:59 +0800

    Update stellarUtils.js

--- client/src/utils/stellarUtils.js
+++ client/src/utils/stellarUtils.js
@@ -1,4 +1,15 @@
-import { Horizon, Asset } from '@stellar/stellar-sdk';
+import {
+  Horizon,
+  TransactionBuilder,
+  Asset,
+  Networks,
+  SorobanRpc,
+  nativeToScVal,
+  Address,
+  BASE_FEE,
+  Contract,
+} from '@stellar/stellar-sdk';
+import { getPublicKey, signTransaction } from '@stellar/freighter-api';
 
 // Function to get XLM balance
 export const getXlmBalance = async walletAddress => {
@@ -30,5 +41,73 @@ export const getUsdcBalance = async walletAddress => {
   } catch (error) {
     console.error('Error fetching USDC balance:', error);
     return '0';
+  }
+};
+
+// Function to send USDC to the contract
+export const sendUSDCToContract = async (contractAddress, sent_amount) => {
+  const contract = new Contract(contractAddress);
+  const NETWORK_PASSPHRASE = Networks.TESTNET;
+  const server = new SorobanRpc.Server(
+    'https://soroban-testnet.stellar.org:443'
+  );
+  let pubKey = await getPublicKey();
+  const sourceAccount = await server.getAccount(pubKey);
+
+  const sentAmount = nativeToScVal(sent_amount, { type: 'i128' });
+  const params = [new Address(pubKey).toScVal(), sentAmount];
+
+  let transaction = new TransactionBuilder(sourceAccount, {
+    fee: BASE_FEE,
+    networkPassphrase: NETWORK_PASSPHRASE,
+  })
+    .addOperation(contract.call('deposit', ...params))
+    .setTimeout(300)
+    .build();
+
+  transaction = await server.prepareTransaction(transaction);
+  console.log(transaction);
+
+  try {
+    let signed_tx_xdr = await signTransaction(
+      transaction.toEnvelope().toXDR('base64'),
+      {
+        network: 'TESTNET',
+        networkPassphrase: NETWORK_PASSPHRASE,
+        accountToSign: pubKey,
+      }
+    );
+    console.log('tx signed');
+
+    let signed_tx = TransactionBuilder.fromXDR(
+      signed_tx_xdr,
+      NETWORK_PASSPHRASE
+    );
+    console.log('tx read from XDR');
+
+    let response = await server.sendTransaction(signed_tx);
+
+    if (response.status !== 'PENDING') {
+      throw new Error(response.errorResultXdr);
+    }
+
+    let getResponse = await server.getTransaction(response.hash);
+    const retryWait = 1000; // 1 sec
+    let retries = 10;
+
+    while (getResponse.status === 'NOT_FOUND' && retries > 0) {
+      await new Promise(resolve => setTimeout(resolve, retryWait));
+      getResponse = await server.getTransaction(response.hash);
+      retries -= 1;
+    }
+
+    if (getResponse.status !== 'SUCCESS') {
+      throw new Error(`Transaction failed: ${getResponse.resultXdr}`);
+    }
+
+    console.log('Deposit Successful!');
+  } catch (e) {
+    console.error('Error invoking contract:', e);
+    throw new Error(e);
   }
 };
